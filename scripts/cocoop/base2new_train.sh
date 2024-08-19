1d4d6c7f3a00597e47dac4f8f28dbf1270bf6817
Huang <81674940+hf618@users.noreply.github.com>
2024-08-20 02:33:03 +0800

    Add files via upload

--- scripts/cocoop/base2new_train.sh
+++ scripts/cocoop/base2new_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ../..
+#cd ../..
 
 # custom config
 DATA=/root/autodl-tmp/dataset/tta_data
@@ -12,14 +12,11 @@ SEED=$2
 
 CFG=vit_b16_c4_ep10_batch1_ctxv1
 # CFG=vit_b16_ctxv1  # uncomment this when TRAINER=CoOp
+# CFG=vit_b16_ep50_ctxv1  # uncomment this when TRAINER=CoOp and DATASET=imagenet
 SHOTS=16
-LOADEP=10
-SUB=new
 
 
-COMMON_DIR=${DATASET}/shots_${SHOTS}/${TRAINER}/${CFG}/seed${SEED}
-MODEL_DIR=output/${TRAINER}/base2new/train_base/${COMMON_DIR}
-DIR=output/${TRAINER}/base2new/test_${SUB}/${COMMON_DIR}
+DIR=output/${TRAINER}/base2new/train_base/${DATASET}/shots_${SHOTS}/${TRAINER}/${CFG}/seed${SEED}
 if [ -d "$DIR" ]; then
     echo "Oops! The results exist at ${DIR} (so skip this job)"
 else
@@ -30,9 +27,6 @@ else
     --dataset-config-file ensemble/CoOp/configs/datasets/${DATASET}.yaml \
     --config-file ensemble/CoOp/configs/trainers/${TRAINER}/${CFG}.yaml \
     --output-dir ${DIR} \
-    --model-dir ${MODEL_DIR} \
-    --load-epoch ${LOADEP} \
-    --eval-only \
     DATASET.NUM_SHOTS ${SHOTS} \
-    DATASET.SUBSAMPLE_CLASSES ${SUB}
+    DATASET.SUBSAMPLE_CLASSES base
 fi
